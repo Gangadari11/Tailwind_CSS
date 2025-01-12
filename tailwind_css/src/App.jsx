5cce6e105b6a7fadda5c7fadd6af4bb80e55c1d4
Sansala Gangadari <slicchandrika@gmail.com>
2025-01-12 18:41:50 +0530

    Complete

---
+++ tailwind_css/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <section className="m-12 flex h-screen items-center justify=center  bg-twitter-blue
+      text-center text-3xl md:p-14 lg:p-16 xl:p-20">
+        <p> Hola, cómo estás? Hoy es un día muy bonito. 
+          Vamos a caminar por el parque y después a tomar un 
+          café en el centro. Qué te parece?
+        </p>
+      </section> */}
+
+      <div className="card rounded-none"> This is a card </div>
+    </div>
+  )
+}
+
+export default App
+ 
